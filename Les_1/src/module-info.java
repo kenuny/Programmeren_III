943b8a1390571d7c81ed169085e209debd1c770e
DeSmetElias <elias.desmet@student.hogent.be>
2020-09-22 13:47:59 +0200

    Les 1

---
+++ Les_1/src/module-info.java
@@ -0,0 +1,2 @@
+module Les_1 {
+}
---
+++ Les_1/src/ui/InleidingDoorlopenCollectie.java
@@ -0,0 +1,20 @@
+package ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InleidingDoorlopenCollectie {
+
+	public static void main(String[] args) {
+		List<String> woorden = new ArrayList<>();
+		woorden.add("Eerste");
+		woorden.add("Tweede");
+		woorden.add("Derde");
+		woorden.add("Eerste");
+		woorden.add("Tweede");
+		woorden.add("Derde");
+		
+		
+		
+	}
+}
