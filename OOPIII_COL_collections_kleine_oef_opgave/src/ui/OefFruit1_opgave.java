f79c2a55508fec9385a61fb7920d15996bb16669
DeSmetElias <elias.desmet@student.hogent.be>
2020-09-29 15:34:06 +0200

    Aanpassen comments

--- OOPIII_COL_collections_kleine_oef_opgave/src/ui/OefFruit1_opgave.java
+++ OOPIII_COL_collections_kleine_oef_opgave/src/ui/OefFruit1_opgave.java
@@ -8,13 +8,13 @@ public class OefFruit1_opgave {
         String arX[] = {"appel", "peer", "citroen", "kiwi"},
                 arY[] = {"banaan", "mango", "citroen", "kiwi", "zespri"};
 
-        /*
-	Behandel arX en arY als Collections en maak gebruik van de bulk
-	peraties om volgende output te leveren:
-		In y zit extra [banaan, mango, zespri]
-		In x zit extra [appel, peer]
-		x en y hebben gemeenschappelijk [citroen, kiwi]
-         */
+    /*
+     * Behandel arX en arY als Collections en maak gebruik van de bulk
+     * operaties om volgende output te leveren:
+     * In y zit extra [banaan, mango, zespri]
+     * In x zit extra [appel, peer]
+     * x en y hebben gemeenschappelijk [citroen, kiwi]
+    */
         
         
     }
