d184eafffdb4074978482cba5973dc621f714a09
DeSmetElias <elias.desmet@student.hogent.be>
2020-10-07 00:20:41 +0200

    fix eclipse sync problem

---
+++ Projecten/OOPIII_DAT_MyList_generiek_lab_start/OOPIII_DAT_MyList_generiek_lab_start/src/testen/AllTests.java
@@ -0,0 +1,13 @@
+package testen;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses({TestLeesBestand.class, TestMyListGeneriek.class,
+	TestMyListIterableGeneriek.class, TestQueueGeneriek.class,
+	TestStackGeneriek.class})
+public class AllTests {
+
+}
