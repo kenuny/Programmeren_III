c6938a0d4adcaf0ca7f37cd9ce03c4d7dc692abf
DeSmetElias <elias.desmet@student.hogent.be>
2020-10-09 21:17:17 +0200

    Update OefFruit1

--- Projecten/H16_T_Collections/src/ui/OefFruit1_opgave.java
+++ Projecten/H16_T_Collections/src/ui/OefFruit1_opgave.java
@@ -5,37 +5,48 @@ import java.util.*;
 public class OefFruit1_opgave {
 
     public static void main(String args[]) {
-        String arX[] = {"appel", "peer", "citroen", "kiwi"},
-                arY[] = {"banaan", "mango", "citroen", "kiwi", "zespri"};
+        String[] x_array = {"appel", "peer", "citroen", "kiwi"};
+        String[] y_array = {"banaan", "mango", "citroen", "kiwi", "zespri"};
 
-    /*
-     * Behandel arX en arY als Collections en maak gebruik van de bulk
-     * operaties om volgende output te leveren:
-     * In y zit extra [banaan, mango, zespri]
-     * In x zit extra [appel, peer]
-     * x en y hebben gemeenschappelijk [citroen, kiwi]
-    */
+      
+        // Maak van de arrays x_array en y_array een statische lijst.
+        Collection<String> x_statischeLijst = Arrays.asList(x_array);
+        Collection<String> y_statischeLijst = Arrays.asList(y_array);
+                
+        // Maak van de statische lijst een dynamische lijst.
+        List<String> x_dynamischeLijst = new ArrayList<>(x_statischeLijst);
+        List<String> y_dynamischeLijst = new ArrayList<>(y_statischeLijst);
         
-        Collection<String> x = Arrays.asList(arX); // Wordt een statische lijst gemaakt van arX genaamd x.
-        Collection<String> y = Arrays.asList(arY); // Wordt een statische lisjt gemaakt van arY genaamd y.
-        
-        List<String> res1 = new ArrayList<>(y); // De statische lijst wordt een dynamische lijst d.m.v. ArrayList genaamd res1
-        res1.removeAll(x); 
-        System.out.printf("In arY zonder arX zit: %s%n", res1);
+        // Maak de output [banaan, mango, zespri].
+        // Door elk element dat in x_dynamischeLijst zit wordt verwijdert uit y_dynamischeLijst
+        y_dynamischeLijst.removeAll(x_statischeLijst); 
+        System.out.printf("In y_dynamischeLijst zonder x_dynamischeLijst zit: %s%n%n", y_dynamischeLijst);
 
-        
-        List<String> res2 = new ArrayList<>(x);
-        res2.removeAll(y);
-        System.out.printf("In arX zonder arY zit: %s%n", res2);
+        // Maak de output [appel, peer].
+        // Door elk element dat in y_dynamischeLijst zit wordt verwijdert uit x_dynamischeLijst
+        x_dynamischeLijst.removeAll(y_statischeLijst);
+        System.out.printf("In x_dynamischeLijst zonder y_dynamischeLijst zit: %s%n%n", x_dynamischeLijst);
       
         
-        List<String> res3 = new ArrayList<>(x);
-        res3.retainAll(y);
-        System.out.printf("arX en arY hebben gemeenschappelijk: %s%n", res3);
+        // Maak een nieuwe dynamische lijst die de gemeenschappelijke 
+        // objecten in x_dynamischeLijst en y_dynamischeLijst zitten.
+        List<String> gemeenschappelijkeObjecten = new ArrayList<>(x_statischeLijst);
+        gemeenschappelijkeObjecten.retainAll(y_statischeLijst);
+        System.out.printf("x_dynamischeLijst en y_dynamischeLijst hebben gemeenschappelijk: %s%n%n", gemeenschappelijkeObjecten);
         
         
-        
-
+        /*
+         * Bewerkingen:
+         * 		Arrays.asList(array1)    -> Zet een array om in een statische lijst.
+         * 		lijst1.removeAll(lijst2) -> verwijdert alle elementen in lijst1 die in lijst2 zitten.
+         * 		lijst1.retainAll(lijst2) -> verwijdert alle elementen in lijst1 die niet in lijst2 zitten.
+         * 		List<String> lijst__dynamisch = new ArrayList<>(lijst__statisch) -> maakt van een statische lijst een dynamische lijst.
+         * 
+         * Verschil statische lijst en dynamische lijst:
+         * 		Op een statische lijst kunnen enkel statische bewerkingen gedaan worden zoals System.out.print() en size().
+         *  	Maar op een statische lijst kan geen bewerking zoals remove() of contains(). 
+         *  	Op een dynamische lijst zijn zowel statische als dynamische bewerkingen mogelijk. (Werk steeds met dynamische lijsten).
+		*/
         
     }
 }
