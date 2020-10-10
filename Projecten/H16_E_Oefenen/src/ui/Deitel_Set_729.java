952149907c7f159d4e249478ee09cd32d0a2e6fe
DeSmetElias <elias.desmet@student.hogent.be>
2020-10-11 00:50:36 +0200

    Finish TreeSet

--- Projecten/H16_E_Oefenen/src/ui/Deitel_Set_729.java
+++ Projecten/H16_E_Oefenen/src/ui/Deitel_Set_729.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Deitel_Set_729 {
 
@@ -20,8 +21,13 @@ public class Deitel_Set_729 {
 		List<String> list = Arrays.asList(colors);
 		System.out.println("List of colors without set: " + list + ".\n");
 
-		Set<String> set = new HashSet<>(list);
-		System.out.println("List of colors with set:    " + set + ".\n");
+		// Voorbeeld van HashSet().
+		Set<String> set_hash = new HashSet<>(list);
+		System.out.println("List of colors with set:    " + set_hash + ".\n");
+
+		// Voorbeeld van TreeSet().
+		Set<String> set_tree = new TreeSet<>(list);
+		System.out.println("List of colors with set:    " + set_tree + ".\n");
 
 	}
 
