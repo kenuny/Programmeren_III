27823eba16054c91ee350c3285e97331e0543c46
DeSmetElias <elias.desmet@student.hogent.be>
2020-10-10 16:36:38 +0200

    Format

--- Projecten/H16_E_Oefenen/src/ui/Deitel_SortingList_717.java
+++ Projecten/H16_E_Oefenen/src/ui/Deitel_SortingList_717.java
@@ -10,8 +10,7 @@ public class Deitel_SortingList_717 {
 	public static void main(String[] args) {
 
 		/*
-		 * Een lijst sorteren door middel van Collections.sort(lijst).
-		 * -Ascending-
+		 * Een lijst sorteren door middel van Collections.sort(lijst). -Ascending-
 		 */
 
 		String[] suits = { "Hearths", "Diamonds", "Clubs", "Spades" };
@@ -21,8 +20,7 @@ public class Deitel_SortingList_717 {
 
 		System.out.println("Collections.sort(lijst)");
 		System.out.println("-----------------------");
-		
-		
+
 		System.out.printf("%nOngesorteerde lijst: %s%n", lijst);
 
 		Collections.sort(lijst);
@@ -31,19 +29,18 @@ public class Deitel_SortingList_717 {
 		/*
 		 * Een lijst sorteren door middel van een comparable.
 		 */
-		
+
 		String[] suits2 = { "Hearths", "Diamonds", "Clubs", "Spades" };
-		
-		List<String>lijst2 = Arrays.asList(suits2);
-		
+
+		List<String> lijst2 = Arrays.asList(suits2);
+
 		System.out.println("\n\nCollections.sort(lijst, comperator (= collections.reverseOrder())");
 		System.out.println("-----------------------------------------------------------------\n");
-		
+
 		System.out.printf("Ongesorteerde lijst: %s", lijst2);
-		
+
 		Collections.sort(lijst2, Collections.reverseOrder());
 		System.out.printf("%nGesorteerde lijst: %s%n", lijst2);
-
 
 	}
 }
