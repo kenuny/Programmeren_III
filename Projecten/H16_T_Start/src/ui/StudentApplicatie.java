250437a1ebb1a727b06d2aa166bd0d37fd4b4ac8
DeSmetElias <elias.desmet@student.hogent.be>
2020-10-11 14:22:49 +0200

    Update

--- Projecten/H16_T_Start/src/ui/StudentApplicatie.java
+++ Projecten/H16_T_Start/src/ui/StudentApplicatie.java
@@ -1,7 +1,5 @@
 package ui;
 
-
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
@@ -11,30 +9,41 @@ import java.util.TreeSet;
 
 import domein.Student;
 
-public class StudentApplicatie
-{
+public class StudentApplicatie {
 
-    public static void main(String[] args)
-    {
-        List<Student> lijstStudenten = new ArrayList<>();
+	public static void main(String[] args) {
 
-        
-        lijstStudenten.add(new Student(20132566,"Janssens","Wendy","Eke"));
-        lijstStudenten.add(new Student(20132567,"Janssens","Hans","Oudenaarde"));
-        lijstStudenten.add(new Student(20132563,"Janssens","Jan","Gent"));
-        lijstStudenten.add(new Student(20132564,"Karels","Matt","Gent"));
-        lijstStudenten.add(new Student(20132565,"Fransen","Luc","Gent"));
-         
-        System.out.println(lijstStudenten);
-        
-        Collections.sort(lijstStudenten, new StudentSorterenVolgensNaamEnVoornaam());
-        System.out.println("gesorteerd volgens naam: " + lijstStudenten);
-        
-         Collections.sort(lijstStudenten);
-         System.out.println("gesorteerd: " + lijstStudenten);
-        
-        // natuurlijke sortering
-        // afwijken kan door null te vervangen door new StudentSorterenVolgensNaamEnVoornaam
-        lijstStudenten.sort(null);
-    }
+		/*
+		 * ArrayList: geschikt wanneer elementen vaak worden opgezocht of achteraan toegevoegd.
+		 * LinkedList: geschikt wanneer elementen vaak worden tussengevoegd of verwijderd.
+		 * 
+		 * Grootte van een lijst: size().
+		 * Grootte van een array: length().
+		 * 
+		 * natuurlijke sortering: list.sort(null)
+		 * 
+		 */
+		List<Student> lijstStudenten = new ArrayList<>();
+
+		lijstStudenten.add(new Student(20132566, "Janssens", "Wendy", "Eke"));
+		lijstStudenten.add(new Student(20132567, "Janssens", "Hans", "Oudenaarde"));
+		lijstStudenten.add(new Student(20132563, "Janssens", "Jan", "Gent"));
+		lijstStudenten.add(new Student(20132564, "Karels", "Matt", "Gent"));
+		lijstStudenten.add(new Student(20132565, "Fransen", "Luc", "Gent"));
+
+		System.out.println(lijstStudenten);
+		
+		System.out.println("");
+
+
+		Collections.sort(lijstStudenten, new StudentSorterenVolgensNaamEnVoornaam());
+		System.out.println("gesorteerd (comparator: naam): " + lijstStudenten);
+		
+		System.out.println("");
+
+		Collections.sort(lijstStudenten);
+		System.out.println("gesorteerd: (automatisch: key) " + lijstStudenten);
+
+		lijstStudenten.sort(null);
+	}
 }
