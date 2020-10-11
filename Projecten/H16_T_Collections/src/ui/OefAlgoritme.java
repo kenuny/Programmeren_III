afc4798460d5dc741e6cbfa1445283199dd58292
DeSmetElias <elias.desmet@student.hogent.be>
2020-10-11 15:01:55 +0200

    Finish OefAlgoritme

--- Projecten/H16_T_Collections/src/ui/OefAlgoritme.java
+++ Projecten/H16_T_Collections/src/ui/OefAlgoritme.java
@@ -35,74 +35,46 @@ public class OefAlgoritme {
 		boeken.add(new Boek("C++", 2200000000L));
 		boeken.add(new Boek("C++", 8800000000L));
 
-		// Sorteer de arrayList op titel in STIJGENDE volgorde. Bij gelijke titels
-		// sorteren op isbn_nr in DALENDE volgorde.
-		// ---------------------------------------------------------------
-		// Java 7:
-		// Collections.sort(boeken, new BoekComparator());
-		
-		// Java 8:
-		//boeken.sort((boek1, boek2) -> {
-		// 	int boekCompare = boek1.getTitel().compareTo(boek2.getTitel());
-		//	if (boekCompare != 0) {
-		//		return boekCompare;
-		//	}
-		//	
-		//	return Long.compare(boek2.getIsbn_nr(), boek1.getIsbn_nr());
-		//	
-		//});
-		
-		// !!WERKT NIET!! Betere oplossing in Java 8:
-//		boeken.sort(
-//				Comparator.comparing(Boek::getTitel)
-//						  .thenComparing(Comparator.comparing(Boek::getIsbn_nr.reversed()));
+		/*
+		 * Sorteert de arrayList op titel in STIJGENDE volgorde. Bij gelijke titels
+		 * sorteren op isbn_nr in DALENDE volgorde.
+		 */
+		boeken.sort(
+				Comparator.comparing(Boek::getTitel).thenComparing(Comparator.comparing(Boek::getIsbn_nr).reversed()));
 
-		boeken.sort(Comparator.comparing(Boek::getTitel).thenComparing(Comparator.comparing(Boek::getIsbn_nr).reversed()));
-		
-		System.out.println("gesorteerd : ");
+		System.out.println("Gesorteerd: ");
 		toonLijst(boeken);
+		System.out.println();
 
-		// Zet de arrayList om in omgekeerde volgorde (= 1 instructie)
-		// ---------------------------------------------------------
-		// java7
-
-		System.out.println("omgekeerde volgorde : ");
+		/*
+		 * Toont de geordende lijst boeken in omgekeerde volgorde.
+		 */
+		System.out.println("Omgekeerde volgorde : ");
+		Collections.reverse(boeken);
 		toonLijst(boeken);
+		System.out.println();
 
+		/*
+		 * Toont het grootste element van de array getallen. Toont het kleinste element
+		 * van de array getallen.
+		 */
 		Integer getallen[] = { 6, 5, 9, 3 };
-		// Toon het grootste element van de array getallen
-		// -----------------------------------------------
-		// java7
-		// System.out.printf("max = %d (oplossing = 3)%n", Collections.max(Arrays.asList(getallen)));
-		
-		// java 8: beiden geven hetzelfde resultaat in dit geval is stream langer.
-		System.out.printf("max = %d (oplossing = 3)%n", Arrays.stream(getallen).max(Integer::compare).get());
-		
-		
-		// Toon het kleinste element van de array getallen
-		// -----------------------------------------------
-		// java7
-		// System.out.printf("max = %d (oplossing = 3)%n", Arrays.stream(getallen).min(Integer::compare).get());
+		System.out.printf("De minimum waarde is %d.%n", Collections.min(Arrays.asList(getallen)));
+		System.out.printf("De maximum waarde is %d.%n", Collections.max(Arrays.asList(getallen)));
 
-		// Java 8: geeft zelfde resultaat als 7
-		System.out.printf("max = %d (oplossing = 3)%n", Arrays.stream(getallen).min(Integer::compare).get());
+		// Java 8:
+		System.out.println("De minimum waarde met Java 8:  " + Arrays.stream(getallen).min(Integer::compare).get());
+		System.out.println("De maximum waarde met Java 8:  " + Arrays.stream(getallen).max(Integer::compare).get());
 
 	}
 
 	public <E> void toonLijst(Collection<E> collection) {
-		// java7
-		//for (E element : collection) {
-		//	System.out.printf("%s%n", element);
-		//}
-
-		//System.out.println("\n");
-		
-		// Java 8
-		collection.forEach(element -> System.out.printf("%s%n", element));
+		// Toont elk element van de meegegeven collection.
+		collection.forEach(e -> System.out.printf("%s%n", e));
 	}
 
 	public static void main(String args[]) {
 		new OefAlgoritme();
 	}
 
-}// einde klasse OefAlgoritme_Opgave
+}
