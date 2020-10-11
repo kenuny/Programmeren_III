1f5eaaab0864d098cdf1ed14468f406099e95d72
DeSmetElias <elias.desmet@student.hogent.be>
2020-10-11 14:49:26 +0200

    Update

--- Projecten/H16_T_Start/src/ui/SynchronizedCollectie_vb.java
+++ Projecten/H16_T_Start/src/ui/SynchronizedCollectie_vb.java
@@ -5,14 +5,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class SynchronizedCollectie_vb
-{
-    public static void main(String[] args)
-    {
-        /*Een programma dat financiële transacties voor de klanten
-        * van een bank verricht kan uit meerdere threads bestaan
-        * om verschillende klanten tegelijkertijd hun transactie te laten uitvoeren.*/
-        List<Rekening> lijst = 
-                Collections.synchronizedList(new ArrayList<>());
-    }
+public class SynchronizedCollectie_vb {
+	public static void main(String[] args) {
+
+		List<Rekening> lijst = Collections.synchronizedList(new ArrayList<>());
+		System.out.println(lijst);
+
+	}
 }
