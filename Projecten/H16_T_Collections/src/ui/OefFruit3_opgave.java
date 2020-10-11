3d4719a05f4821dddcc81aa45468a384175ac96d
DeSmetElias <elias.desmet@student.hogent.be>
2020-10-11 15:24:05 +0200

    Finsih

--- Projecten/H16_T_Collections/src/ui/OefFruit3_opgave.java
+++ Projecten/H16_T_Collections/src/ui/OefFruit3_opgave.java
@@ -1,12 +1,12 @@
 package ui;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 public class OefFruit3_opgave {
 
@@ -15,56 +15,69 @@ public class OefFruit3_opgave {
 				{ "banaan", "mango", "citroen", "kiwi", "zespri", "pruim" },
 				{ "peche", "lichi", "kriek", "kers", "papaya" } };
 
-		List<String> list;
+		List<String> list = new ArrayList<>();
 
-		// list = new ArrayList<>();
-		// java7
 		/*
-		 * for (String[] eenKist : kist) { list.addAll(Arrays.asList(eenKist)); }
+		 * Plaatst elk element van de 2D array in de lijst. Manier 1: enhanced-for
+		 * Manier 2: stream.forEach() Manier 3: stream.flatMap()
 		 */
+
+		// Manier 1
+		for (String[] eenKist : kist) {
+			list.addAll(Arrays.asList(eenKist));
+		}
+
+		// Manier 2:
 		// Arrays.stream(kist).forEach(eenkist -> list.addAll(Arrays.asList(eenkist)));
-		list = Arrays.stream(kist).flatMap(Arrays::stream).collect(Collectors.toList());
+
+		// Manier 3:
+		// list =
+		// Arrays.stream(kist).flatMap(Arrays::stream).collect(Collectors.toList());
+
+		System.out.println(list);
 
 		/*
-		 * Berg de fruit list van vorige oefeningen in een boom op zodat dubbels
-		 * ge�limineerd worden. Er moet ook de mogelijkheid zijn de bijhorende prijs
-		 * (decimale waarde) bij te houden.
+		 * Maakt een TreeMap (geen duplicaten map) aan. Plaatst elk fruit als key in de
+		 * map en geeft als value null.
+		 * 
+		 * Manier 1: enhanced for Manier 2: Lambda
 		 */
-		// ------------------------------------------------------------
-		/*
-		 * Doorloop de boom in lexicaal oplopende volgorde en vraag telkens de
-		 * bijhorende prijs, die je mee in de boom opbergt.
-		 */
-		// ------------------------------------------------------------
-		/*
-		 * Druk vervolgens de volledige lijst in twee kolommen (naam : prijs) in
-		 * lexicaal oplopende volgorde af op het scherm.
-		 */
-		// ------------------------------------------------------------
+		Scanner scan = new Scanner(System.in);
 		Map<String, Double> fruit = new TreeMap<>();
 
-		Scanner in = new Scanner(System.in);
-		/*
-		 * for (String eenFruit : list) { fruit.put(eenFruit, null); }
-		 */
-		list.forEach(eenFruit -> fruit.put(eenFruit, null));
+		// Manier 1:
+		for (String eenFruit : list) {
+			fruit.put(eenFruit, null);
+		}
+
+		// Manier 2:
+		// list.forEach(eenFruit -> fruit.put(eenFruit, null));
 
 		/*
-		 * for (Map.Entry<String, Double> eenEntry : fruit.entrySet()) {
-		 * System.out.printf("Prijs van %s : ", eenEntry.getKey()); double prijs =
-		 * in.nextDouble(); eenEntry.setValue(prijs); }
+		 * Vraagt per key aan de gebruiker de prijs. De prijs wordt vervolgens
+		 * opgeslagen als value.
+		 * 
+		 * output vervolgens de map.
 		 */
-		fruit.entrySet().forEach(eenEntry -> {
-			System.out.printf("Prijs van %s : ", eenEntry.getKey());
-			double prijs = in.nextDouble();
+
+		for (Map.Entry<String, Double> eenEntry : fruit.entrySet()) {
+			System.out.printf("Prijs van %s: ", eenEntry.getKey());
+			double prijs = scan.nextDouble();
 			eenEntry.setValue(prijs);
-		});
+		}
 
-		fruit.forEach((naam, prijs) -> System.out.println(naam + "\t" + prijs));
-		/*
-		 * for (Map.Entry<String, Double> eenEntry : fruit.entrySet()) {
-		 * System.out.println(eenEntry.getKey() + "\t" + eenEntry.getValue()); }
-		 */
+		System.out.println();
+
+		// Door middel van Lambda
+		// fruit.entrySet().forEach(eenEntry -> {
+		// System.out.printf("Prijs van %s : ", eenEntry.getKey());
+		// double prijs = scan.nextDouble();
+		// eenEntry.setValue(prijs);
+		// });
+
+		System.out.printf("%-15s%s%n", "Fruitsoort", "Prijs in €");
+
+		fruit.forEach((fruitsoort, prijs) -> System.out.printf("%-15s%s%n", fruitsoort, prijs));
 
 	}
 
@@ -79,3 +92,9 @@ public class OefFruit3_opgave {
 	}
 
 }
+
+///*
+// * for (Map.Entry<String, Double> eenEntry : fruit.entrySet()) {
+// * System.out.println(eenEntry.getKey() + "\t" + eenEntry.getValue()); }
+// */
+//fruit.forEach((naam, prijs) -> System.out.println(naam + "\t" + prijs));
