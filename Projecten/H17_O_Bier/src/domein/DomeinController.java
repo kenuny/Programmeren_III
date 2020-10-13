fe19d98951ccc8b3a8ed0acece632d968a133b40
DeSmetElias <elias.desmet@student.hogent.be>
2020-10-13 15:46:19 +0200

    Finish applicatie Bier

--- Projecten/H17_O_Bier/src/domein/DomeinController.java
+++ Projecten/H17_O_Bier/src/domein/DomeinController.java
@@ -16,17 +16,16 @@ public class DomeinController {
 		return bierWinkel.geefAantalBierenMetMinAlcoholPercentage(percentage);
 	}
 
-	public List<String> geefLijstAlleBierenMetMinAlcoholPercentage(double percentage)
-    {
-    	
-    	List<Bier> bieren = bierWinkel.geefAlleBierenMetMinAlcoholPercentage(percentage);
-    	
-    	// lambda method
-    	return bieren.stream().map(bier -> bier.toString()).collect(Collectors.toList());
-    	
-    	// method reference
-    	// return bieren.stream().map(Bier::toString).collect(Collectors.toList());
-    }
+	public List<String> geefLijstAlleBierenMetMinAlcoholPercentage(double percentage) {
+
+		List<Bier> bieren = bierWinkel.geefAlleBierenMetMinAlcoholPercentage(percentage);
+
+		// lambda method
+		return bieren.stream().map(bier -> bier.toString()).collect(Collectors.toList());
+
+		// method reference
+		// return bieren.stream().map(Bier::toString).collect(Collectors.toList());
+	}
 
 	public List<String> geefAlleBieren() {
 		return bierWinkel.getBieren().stream().map(Bier::toString).collect(Collectors.toList());
@@ -47,7 +46,7 @@ public class DomeinController {
 
 	public List<String> geefGeordendOpAlcoholGehalteEnNaam() {
 		List<Bier> bieren = bierWinkel.geefGeordendOpAlcoholGehalteEnNaam();
-		
+
 		return bieren.stream().map(Bier::toString).collect(Collectors.toList());
 	}
 
@@ -58,18 +57,19 @@ public class DomeinController {
 	public String geefAlleNamenBrouwerijenMetWoord(String woord) {
 		return bierWinkel.geefAlleNamenBrouwerijenMetWoord(woord).stream().collect(Collectors.joining("\n"));
 	}
-
+	
 	public String opzettenAantalBierenPerSoort() { // naar BierWinkel --> map<String, Long>
-		return null;
+		return overzichtToString(bierWinkel.opzettenAantalBierenPerSoort());
 	}
 
 	public String opzettenOverzichtBierenPerSoort() { // naar BierWinkel --> map<String, List<Bier>>
-		return null;
+		return overzichtToString(bierWinkel.opzettenOverzichtBierenPerSoort());
 	}
 
-	/*
-	 * private String overzichtToString(Map< > map) { //hulp voor map --> String
-	 * return null; }
-	 */
-
+	// typeparametersectie <K, V> komt steeds voor de retourwaarde.
+	private <K, V> String overzichtToString(Map<K, V> map) { // hulp voor map --> String
+		return map.entrySet().stream()
+							 .map(element -> String.format("%s: %s", element.getKey(), element.getValue()))
+							 .collect(Collectors.joining("\n"));
+	}
 }
