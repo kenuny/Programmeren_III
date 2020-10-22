8fa6ef4224e47cc073042405d7a65d869de16284
DeSmetElias <elias.desmet@student.hogent.be>
2020-10-22 20:01:44 +0200

    add

---
+++ Projecten/H20_T_ExtraTheorie/src/cui/ToepassingGeneriekeKlasseRaw.java
@@ -0,0 +1,27 @@
+package cui;
+
+import domein.GeneriekeKlasse;
+import domein.GeneriekeKlasseMetUpperBound;
+import domein.Student;
+
+public class ToepassingGeneriekeKlasseRaw {
+
+	public static void main(String[] args) 
+	{
+		GeneriekeKlasse<Student> student1 = new GeneriekeKlasse<>(new Student(20132566,"Janssens","Wendy","Eke"));
+
+		System.out.println(student1.getEersteAttribuut());
+		System.out.print(student1.getEersteAttribuut().getStamboeknr());
+		
+		GeneriekeKlasse<Student> student2 = new GeneriekeKlasse(new Student(20132566,"Janssens","Wendy","Eke"));
+
+		System.out.println(student2.getEersteAttribuut());
+		System.out.print(student2.getEersteAttribuut().getStamboeknr());
+		
+		GeneriekeKlasse student3 = new GeneriekeKlasse<Student>(new Student(20132566,"Janssens","Wendy","Eke"));
+
+		System.out.println(student3.getEersteAttribuut());
+		// System.out.print(student3.getEersteAttribuut().getStamboeknr());
+	}
+
+}
