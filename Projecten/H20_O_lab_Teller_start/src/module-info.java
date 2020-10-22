5be561da305858e3a1946a64bc465fa545f6d9f6
DeSmetElias <elias.desmet@student.hogent.be>
2020-10-22 21:21:33 +0200

    Revert "add projects multithreading"
    This reverts commit b3af081593d0b8122b17ced1caa76c19d981cf5a.

--- Projecten/H20_O_lab_Teller_start/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module OOPIII_MUL_lab_Teller {
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires javafx.base;
-	requires javafx.fxml;
-	opens gui to javafx.graphics, javafx.fxml;
-	opens main to javafx.graphics;
-}
--- Projecten/H20_T_ConsProdMetSyn_ArrayBlockingQueue/src/Buffer.java
+++
@@ -1,6 +0,0 @@
-public interface Buffer {
-	
-	public void set(int value); // place value into Buffer
-
-	public int get(); // return value from Buffer
-}
